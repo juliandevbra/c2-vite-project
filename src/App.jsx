5f2636426402cbe24bfa52669195ae9f13d6450e
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2024-05-08 21:56:00 -0300

    Clase 4: Map y estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
 import './App.css'
-import Button from './Components/Button'
-// import Pepito, {OtroEjemplo} from './Components/Ejemplo'
-import Estudiante from './Components/Estudiante'
+import Home from './Components/Home'
+import Navbar from './Components/Navbar'
+
 
 function App() {
 
-  const borrar = () => {
-    console.log('Se borró el elemento')
-  }
-  const cancelar = () => {
-    console.log('Se canceló la operación')
-  }
-  const aceptar = () => {
-    console.log('Se aceptaron los terminos y condiciones')
-  }
-
-
   return (
     <>
-        <h1>Bienvenidos a FRONTEND III</h1>
-        {/* <Pepito/>
-        <OtroEjemplo/> */}
-        <Estudiante nombre='Joaquin' materia='Infraestructura II' color='verde'/>
-        <Estudiante nombre='Jero' materia='Frontend III' color='rojo'/>
-        <Estudiante nombre='Franceso' materia='Backend' color='plateado'/>
-        <Button texto='Cancelar' funcion={cancelar}/>
-        <Button texto='Borrar' funcion={borrar}/>
-        <Button texto='Aceptar' funcion={aceptar}/>
-        {/* Faltó ejemplo de children */}
+      {/* <h1>Ejemplo css</h1> */}
+      <Navbar/>
+      <Home />
     </>
   )
 }
--- src/Components/Button.jsx
+++
@@ -1,7 +0,0 @@
-const Button = ({texto, funcion}) => {
-  return (
-    <button onClick={funcion}>{texto}</button>
-  )
-}
-
-export default Button
